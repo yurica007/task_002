1b415e19e0e79aaf6bc8ebb8896915e85273e914
ユリカ007 <yudai1023.ta@gmail.com>
2022-10-13 09:33:59 +0900

    FIGHTコマンドの追加

---
+++ task_002/MenuFight.cpp
@@ -0,0 +1,91 @@
+#include "MenuFight.h"
+#include "DxLib.h"
+#include "Pad.h"
+
+namespace
+{
+	// 四角の中央の位置
+	constexpr float kCenterPosX = 640.0f;
+	constexpr float kCenterPosY = 500.0f;
+
+	// 攻撃カーソルの高さと幅
+	constexpr float kCursorHeight = 120.0f;
+	constexpr float kCursorWidth = 20.0f;
+
+	// 攻撃カーソルの移動速度
+	constexpr float kCurcorSpeed = 15.0f;
+}
+
+MenuFight::MenuFight()
+{
+	m_damageCursorCenterX = 0.0f;
+	m_damageCursorPos.x = 0;
+	m_damageCursorPos.y = 0;
+	m_damageCursorVec = 0.0;
+
+	m_dealtDamage = 0.0f;
+	ATK = 0;
+
+	m_isEnd = false;
+}
+
+MenuFight::~MenuFight()
+{
+
+}
+
+void MenuFight::init()
+{
+	m_damageCursorCenterX = 0.0f;
+	m_damageCursorPos.x = 280.0f;
+	m_damageCursorPos.y = kCenterPosY;
+	m_damageCursorVec = kCurcorSpeed;
+
+	m_dealtDamage = 0.0f;
+	ATK = 10;
+}
+
+void MenuFight::end()
+{
+	
+}
+
+void MenuFight::update()
+{
+	m_damageCursorPos.x += m_damageCursorVec;
+	m_damageCursorCenterX = m_damageCursorPos.x + kCursorWidth / 2;
+
+	if (Pad::isPress(PAD_INPUT_2))
+	{
+		m_damageCursorVec = 0.0f;
+		m_isEnd = true;
+
+		if (m_damageCursorCenterX < kCenterPosX)
+		{
+			m_dealtDamage = m_damageCursorCenterX / kCenterPosX;
+		}
+		else
+		{
+			m_dealtDamage = kCenterPosX / m_damageCursorCenterX;
+		}
+
+	}
+
+	if (m_damageCursorPos.x >= 1000.0f)
+	{
+		m_damageCursorVec = 0.0f;
+		m_isEnd = true;
+	}
+}
+
+void MenuFight::draw()
+{
+	DrawFormatString(10, 10, GetColor(255, 255, 255),
+		"攻撃力：%d", ATK);
+
+	DrawFormatString(10, 30, GetColor(255, 255, 255),
+		"ダメージ数：%f", ATK * m_dealtDamage);
+
+	DrawBoxAA(m_damageCursorPos.x, m_damageCursorPos.y - kCursorHeight,
+		m_damageCursorPos.x + kCursorWidth, m_damageCursorPos.y + kCursorHeight, GetColor(255, 0, 0), true);
+}
---
+++ task_002/MenuFight.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Vec2.h"
+#include "SelectMenu.h"
+
+class MenuFight
+{
+public:
+	MenuFight();
+	virtual ~MenuFight();
+
+	virtual void init();
+	virtual void end();
+	virtual void update();
+	virtual void draw();
+
+	virtual bool inEnd() { return m_isEnd; }
+
+private:
+	// 攻撃カーソルの横の座標情報
+	float m_damageCursorCenterX;
+	// 攻撃カーソルの座標情報
+	Vec2 m_damageCursorPos;
+	// 攻撃カーソルの移動速度
+	float m_damageCursorVec;
+
+	// 攻撃力
+	int ATK;
+	// 与えるダメージ量
+	float m_dealtDamage;
+
+	bool m_isEnd;
+};
