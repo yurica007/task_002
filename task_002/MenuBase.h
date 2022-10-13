34361b6fc73c9b49fdc332123d76fc856ac42bb4
ユリカ007 <yudai1023.ta@gmail.com>
2022-10-13 10:55:18 +0900

    新規追加

---
+++ task_002/MenuBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class MenuBase
+{
+public:
+	MenuBase() {}
+	virtual ~MenuBase() {}
+
+	virtual void init() {}
+	virtual void end() {}
+	virtual void update() {}
+	virtual void draw() {}
+
+	virtual bool inEnd() { return false; }
+};
